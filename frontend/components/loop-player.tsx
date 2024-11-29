f66b4b592fa52c264d7420535732a73abe674b78
MagentaManifold <17zhaomingyuan@gmail.com>
2024-11-28 19:57:52 -0500

    feat:create modal for rating

--- frontend/components/loop-player.tsx
+++ frontend/components/loop-player.tsx
@@ -1,6 +1,15 @@
 "use client";
 
-import { Button, Card, CardBody, Link } from "@nextui-org/react";
+import {
+  Button,
+  Card,
+  CardBody,
+  Link,
+  Tooltip,
+  useDisclosure,
+} from "@nextui-org/react";
+
+import { RateModal } from "./rate-modal";
 
 import { Sequencer } from "@/components/sequencer";
 import { LoopInfoJson } from "@/types";
@@ -26,20 +35,31 @@ export const LoopPlayer = ({
     }
   }
 
+  const rateModalState = useDisclosure();
+
   return (
     <>
       <Card>
         <CardBody className="gap-4 flex justify-center">
           <div className="flex flex-row justify-between items-center gap-4">
-            <Button onClick={togglePlaying}>{playing ? "STOP" : "PLAY"}</Button>
-            <p className="text-lg ">
+            <Button onPress={togglePlaying}>{playing ? "STOP" : "PLAY"}</Button>
+            <p className="text-lg">
               <b>{loopInfo.title}</b> by <b>{loopInfo.author}</b>
             </p>
             <p> {loopInfo.bpm} BPM</p>
             <div className="basis-0 flex-grow" />
-            <Button as={Link} href={`/remix/${loopInfo.id}`}>
-              REMIX
-            </Button>
+            <Tooltip closeDelay={50} content="Rate this loop">
+              <Button onPress={rateModalState.onOpen}>Rate</Button>
+            </Tooltip>
+            <RateModal
+              isOpen={rateModalState.isOpen}
+              onOpenChange={rateModalState.onOpenChange}
+            />
+            <Tooltip closeDelay={50} content="Create a remix!">
+              <Button as={Link} href={`/remix/${loopInfo.id}`}>
+                Remix
+              </Button>
+            </Tooltip>
           </div>
           <Sequencer
             bpm={loopInfo.bpm}
